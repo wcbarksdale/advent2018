597422dc481a3e3196625172cd96d490d774100c
Bill Barksdale <bill@thegroundwork.com>
2018-12-04 09:31:13 -0500

    day 2 part 2

--- lib/day2.exs
+++ lib/day2.exs
@@ -14,6 +14,19 @@ defmodule Day2 do
   def checksum(e) do
     Enum.count(e, &has_n_instances(&1, 2)) * Enum.count(e, &has_n_instances(&1, 3))
   end
+
+  def common(e1, e2) do
+    Enum.zip(e1, e2) |> Enum.count(fn ({a, b}) -> a == b end)
+  end
+
+  def almost?(e1, e2) do
+    common(e1, e2) == Enum.count(e1) - 1
+  end
+
+  def almost_matches(ids) do
+    for a <- ids, b <- ids, almost?(a,b), do: {a, b}
+  end
+  
 end
 
 box_ids = File.read!("./day2input.txt") |> String.split |> Enum.map(&String.to_charlist(&1)) |> Enum.to_list
@@ -21,3 +34,7 @@ IO.inspect(Day2.count_values('abcdabcaa'))
 IO.inspect(Day2.has_n_instances('abcdabcaa', 2))
 IO.inspect(Day2.has_n_instances('abcdabcaa', 5))
 IO.inspect(Day2.checksum(box_ids))
+
+IO.inspect(Day2.common([1,2,3],[0,2,3]))
+
+IO.inspect(Day2.almost_matches(box_ids))
